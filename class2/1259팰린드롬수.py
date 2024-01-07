d767dc04d9f9558682c35f361935ac260bae7845
JoWoonJi <153145539+JoWoonJi@users.noreply.github.com>
2024-01-07 18:15:44 +0900

    Update 1259팰린드롬수.py

--- class2/1259팰린드롬수.py
+++ class2/1259팰린드롬수.py
@@ -2,8 +2,7 @@ while True:
     palindrome = input()
     if palindrome == "0":
         break
-    if palindrome == ''.join(reversed(palindrome)):
+    if palindrome == ''.join(reversed(palindrome)):  # 또는 palindrome[::-1] // reversed라는 함수가 있길래 사용해 보았는데 쉬운 방법이 있었다.
         print("yes")
     else:
         print("no")
-    
