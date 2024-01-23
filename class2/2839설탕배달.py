8c91b6d62f3b157989705e1731dc5099a65566e0
JoWoonJi <153145539+JoWoonJi@users.noreply.github.com>
2024-01-23 11:04:56 +0900

    Update 2839설탕배달.py

--- class2/2839설탕배달.py
+++ class2/2839설탕배달.py
@@ -10,3 +10,16 @@ while n >= 0:
     bags += 1
 else:
     print(-1)
+
+#동적 계획법(Dynamic Programming)
+# n = int(input())
+# dp = [5001] * (n + 1)
+# dp[0] = 0
+
+# for i in range(3, n + 1):
+#     if i >= 3:
+#         dp[i] = min(dp[i], dp[i - 3] + 1)
+#     if i >= 5:
+#         dp[i] = min(dp[i], dp[i - 5] + 1)
+
+# print(-1 if dp[n] > 5000 else dp[n])
