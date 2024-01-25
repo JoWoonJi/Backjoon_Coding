5ff86faed36a30e443827b925c2e8cc549a6b3b4
JoWoonJi <153145539+JoWoonJi@users.noreply.github.com>
2024-01-25 15:00:05 +0900

    Add files via upload

---
+++ class2/9012괄호.py
@@ -0,0 +1,18 @@
+t = int(input())
+
+for _ in range(t):
+    ps = input()
+    stack = []
+    vps = 'YES'
+    
+    for c in ps:
+        if c == '(':
+            stack.append(c)
+        elif c == ')':
+            if not stack or stack[-1] != '(':
+                vps = 'NO'
+                break
+            stack.pop()
+    if stack:
+        vps = 'NO'
+    print(vps)
