1b3bdd87d1ee310dc5c82c72ea6ff22bdcf32994
JoWoonJi <153145539+JoWoonJi@users.noreply.github.com>
2024-04-05 10:11:51 +0900

    Add files via upload

---
+++ class1/A10171.java
@@ -0,0 +1,8 @@
+class A10171{
+    public static void main(String[] a){
+    System.out.println("\\    /\\");
+    System.out.println(" )  ( ')");
+    System.out.println("(  /  )");
+    System.out.println(" \\(__)|");
+}
+}
