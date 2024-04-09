ac2326176d61807fff2d79e7919bd16536dd5f92
JoWoonJi <153145539+JoWoonJi@users.noreply.github.com>
2024-04-09 20:29:44 +0900

    Add files via upload

---
+++ class1/A10950.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class A10950{
+    public static void main(String[]z){
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt(),i;
+        for (i=0;i<t;i++){
+            int a=s.nextInt(),b=s.nextInt();
+            System.out.println(a+b);
+        s.close();
+        }
+    }
+}
