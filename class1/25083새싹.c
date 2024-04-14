21e6a78ce470b0884b877e7ba3ecd49e9c914af5
JoWoonJi <153145539+JoWoonJi@users.noreply.github.com>
2024-04-14 10:59:50 +0900

    Add files via upload

---
+++ class1/25083새싹.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    printf("         ,r'\"7\n"
+           "r`-_   ,'  ,/\n"
+           " \\. \" . L_r'\n"
+           "   `~\\/\n"
+           "      |\n"
+           "      |\n");
+}
