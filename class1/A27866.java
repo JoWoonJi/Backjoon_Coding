6684335a61bea045980b723d3e07b4c6b29c6b7e
JoWoonJi <153145539+JoWoonJi@users.noreply.github.com>
2024-04-15 10:07:55 +0900

    Add files via upload

---
+++ class1/A27866.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class A27866{
+    public static void main(String[]z){
+        Scanner a=new Scanner(System.in);
+        String s=a.nextLine();
+        int i=a.nextInt();
+        System.out.print(s.charAt(i-1));
+        a.close();
+    }    
+}
